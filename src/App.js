9cb1a2f750ca310aae2ebf4ae64ccdc33055b30f
chen.shunyi@xenera.jp <chen.shunyi@xenera.jp>
2021-09-14 16:56:09 +0800

    fix: create new sourcecode

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> */}
+        header
+      </header>
+      <body>
+        <div className="App-body">
+            <div className="App-body-left">
+              map area
+            </div>
+            <div className="App-body-right">
+            thumbnail
+            </div>
+
+        </div>
+      </body>
+    </div>
+  );
+}
+
+export default App;
